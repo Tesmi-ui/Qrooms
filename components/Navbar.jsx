b91b798cdca247c4406462031a4138153bd3472f
Pratik <77522616+Tesmi-ui@users.noreply.github.com>
2022-03-21 19:22:16 +0530

    Update Navbar.jsx
    Siteinfo.html<link>removed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ import { FiKey } from 'react-icons/fi';
 const Navbar = () => (
     <Flex p='2' borderBottom='1px' borderColor='gray.100'>
         <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-            <Link href="/siteinfo.html" paddingLeft='2'>Qrooms</Link>
+            <Link href="/" paddingLeft='2'>Qrooms</Link>
         </Box>
         <Spacer />
         <Box>
